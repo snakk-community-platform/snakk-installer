de128ca6def8fa4a42276320a4576e0390b03e0a
Pål Rune S. Tuv <me@paaltuv.no>
2026-05-23 22:42:06 -0600

    image: prometheuscommunity/postgres-exporter:v0.16.0

--- docker/install.sh
+++ docker/install.sh
@@ -1418,7 +1418,7 @@ generate_docker_compose() {
       - "9100"
 
   postgres-exporter:
-    image: quay.io/prometheuscommunity/postgres_exporter:v0.16.0
+    image: prometheuscommunity/postgres-exporter:v0.16.0
     restart: unless-stopped
     depends_on:
       - postgres
